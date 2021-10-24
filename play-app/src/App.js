fcab1f673057a698b677bcf45c26c5781ccd14dc
Julianna <gagliardijulianna@gmail.com>
2021-10-24 17:35:51 -0600

    created basic route paths

--- play-app/src/App.js
+++ play-app/src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Route path="/home" exact component={<Home/>} />
+      <Route path='/default' exact component={<Default/>}/>
+      <Switch>
+        <Route path='/:play' children={<PlayDetails/>}/>
+      </Switch>
     </div>
   );
 }
