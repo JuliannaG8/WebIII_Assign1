e6a85fbd3f1b782294a2bff32c69f018af5c79b2
Julianna <gagliardijulianna@gmail.com>
2021-10-26 12:45:47 -0600

    Moved the useEffect for fetching to DefaultView; uncommented the router link to default view

--- play-app/src/App.js
+++ play-app/src/App.js
@@ -2,32 +2,14 @@ import './App.css';
 import {Route, Switch} from 'react-router-dom';
 import {useState, useEffect} from "react";
 import {useLocalStorage} from "./Hooks/useLocalStorage";
+import DefaultView from "./Components/DefaultView";
 
 function App() {
-    const [plays, updatePlays] = useLocalStorage("plays", [])
-    useEffect(()=> {
-        const url = "https://www.randyconnolly.com//funwebdev/3rd/api/shakespeare/list.php"; //url to fetch data
-        if (plays.length === 0) { //only fetches if local storage doesn't exist
-            fetch(url)
-                .then(response => {
-                    if (response.ok) {
-                        return response.json();
-                    } else {
-                        throw new Error("Fetch failed");
-                    }
-                })
-                .then(data => {
-                    //places fetched data in state
-                    updatePlays(data);
-                })
-                .catch(error => console.error(error));
-        }
-    })
   return (
     <div className="App">
       {/*  commented out for now so nothing breaks while testing*/}
       {/*<Route path="/home" exact component={<Home/>} />*/}
-      {/*<Route path='/default' exact component={<Default/>}/>*/}
+      <Route path='/default' exact component={<DefaultView/>}/>
       {/*<Switch>*/}
       {/*  <Route path='/:play' children={<PlayDetails/>}/>*/}
       {/*</Switch>*/}
