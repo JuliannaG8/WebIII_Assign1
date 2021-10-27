91f90e2cf32f6a00248e988b4825c7e902816b80
Julianna <gagliardijulianna@gmail.com>
2021-10-27 16:13:06 -0600

    added keywords property to PlayList for search functionality

--- play-app/src/Components/DefaultView.js
+++ play-app/src/Components/DefaultView.js
@@ -2,12 +2,13 @@ import {useLocalStorage} from "../Hooks/useLocalStorage";
 import {useState, useEffect} from "react";
 import Loader from 'react-loader-spinner';
 
+
 const DefaultView = (props) =>{
     const [plays, updatePlays] = useLocalStorage("plays", []);
     const [isFetching, stopFetching] = useState(true);
     useEffect(()=> {
         const url = "https://www.randyconnolly.com//funwebdev/3rd/api/shakespeare/list.php"; //url to fetch data
-        if (plays.length() === 0) { //only fetches if local storage doesn't exist
+        if (plays.length === 0) { //only fetches if local storage doesn't exist
             fetch(url)
                 .then(response => {
                     if (response.ok) {
@@ -23,14 +24,18 @@ const DefaultView = (props) =>{
                 })
                 .catch(error => console.error(error));
         } else stopFetching(false);
-    })
-    if (isFetching && plays.length() === 0){
-        return <Loader type="Circles" color="#00BFFF" height={80} width={80}/>
+    }, [plays, updatePlays])
+    if (isFetching && plays.length === 0){
+        return <Loader type="Circles" color="#00BFFF" height="50vh" width="50vh"/>
     } else{
         return (
             <main className="default">
                 {/*<Header/>*/}
-                {/*<PlayList plays={plays} keywords={props.search}/>*/}
+            {/*    {if (props.search){*/}
+            {/*        <PlayList plays={plays} keyword={props.search}/>*/}
+            {/*    }else{*/}
+            {/*    <PlayList plays={plays}/>*/}
+            {/*}}*/}
                 {/*<Favourites/>*/}
             </main>
         )
