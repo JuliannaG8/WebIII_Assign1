a5ac4ff5afcbbb0f5258c21d9e9a6c83c46dc4e0
Julianna <gagliardijulianna@gmail.com>
2021-10-27 16:57:02 -0600

    changes what PlayList renders based on whether props.search is type string

--- play-app/src/Components/PlayList.js
+++ play-app/src/Components/PlayList.js
@@ -1,4 +1,18 @@
-const PlayList = props =>{
 
+const PlayList = props =>{
+    if (typeof props.search === "string"){
+        const filteredPlays = props.plays.filter(p=> p.title.toLowerCase().includes(props.search.toLowerCase()))
+        return(
+            <div>
+                {filteredPlays.map(p=><p>{p.title}</p>)}
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                {props.plays.map(p => <p>{p.title}</p>)}
+            </div>
+        )
+    }
 }
 export default PlayList;
