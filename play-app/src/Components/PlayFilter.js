695f13a5fab8326d48c778b9523e275c2a5c6846
Julianna <gagliardijulianna@gmail.com>
2021-10-28 10:56:57 -0600

    PlayFilter now returns a form element

--- play-app/src/Components/PlayFilter.js
+++ play-app/src/Components/PlayFilter.js
@@ -1,4 +1,22 @@
 const PlayFilter=props=>{
+    const handleSubmit =(e) => {
+
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <h2>Filters</h2>
+            <div>
+                <p><label>Title</label></p>
+            <input name="title" type='text'/>
+            </div>
+            <div>
+                <p><label>Year</label></p>
+                <p><input name="before" type='radio'/>Before <input type="text" name="beforeYear"/></p>
+                <p><input name="after" type='radio'/>After <input type='text' name="afterYear"/></p>
+            </div>
+        </form>
+    )
 
 }
 export default PlayFilter;
