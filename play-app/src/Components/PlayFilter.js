f5bfcd66cb5c207fb1021432ffefcea759a770ec
Julianna <gagliardijulianna@gmail.com>
2021-10-31 18:33:29 -0600

    added comments

--- play-app/src/Components/PlayFilter.js
+++ play-app/src/Components/PlayFilter.js
@@ -1,22 +1,26 @@
 import {createRef} from "react";
 
 const PlayFilter=props=>{
+    //creating references for form fields
     const before = createRef();
     const beforeYear = createRef();
     const after = createRef();
     const afterYear = createRef();
     const title = createRef();
     const genre = createRef();
+
     const handleSubmit =(e) => {
 
         e.preventDefault();
 
         const filters = {};
+        //only adds inputted parameters
         if (before.current.checked && beforeYear.current.value.length > 0) filters.before = beforeYear.current.value;
         if (after.current.checked && afterYear.current.value.length > 0) filters.after = afterYear.current.value;
         if (title.current.value.length > 0) filters.title = title.current.value;
         if (genre.current.value.length > 0) filters.genre = genre.current.value;
-
+        //if no parameters are given, sends an alert,
+        //otherwise calls passed filter function
         if (Object.keys(filters).length === 0)
             alert("Error: please input some filter parameters")
         else {
