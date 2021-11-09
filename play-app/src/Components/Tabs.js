34b4ee61a5b8fb38b1e3ea693ee5ec39f656579e
Julianna <gagliardijulianna@gmail.com>
2021-11-08 21:18:32 -0700

    text tabs work properly

--- play-app/src/Components/Tabs.js
+++ play-app/src/Components/Tabs.js
@@ -1,6 +1,53 @@
 
 const Tabs = props =>{
-    return <p></p>
+    if (props.tabName === "Details"){
+        return(
+            <div>
+                <p>Likely Year: {props.details.likelyDate}</p>
+                <p> Genre: {props.details.genre}</p>
+                <p>Wiki: {props.details.wiki}, Gutenberg: {props.details.gutenberg}, ShakespeareOrg: {props.details.shakespeareOrg}</p>
+                <p>{props.details.desc}</p>
+            </div>
+        )
+    } else if(props.tabName === "Characters"){
+        return (
+            <ul>
+                {props.details.map(p=><li key={p}>{p.player}--------{p.desc}</li>)}
+            </ul>
+        )
+    }
+    else{
+        return (
+            <div>
+                {props.details.map(act=>{
+                    return(
+                        <span>
+                            <hr/>
+                        <h3>{act.name}</h3>
+                        {act.scenes.map(sc=>{
+                            return(
+                                <span>
+                                    <hr/>
+                                    <h3>{sc.name}</h3>
+                                    <p><em>{sc.title}</em></p>
+                                    {sc.speeches.map(sp=>{
+                                        return(
+                                            <div>
+                                                <h3>{sp.speaker}</h3>
+                                                {sp.lines.map(l=><p>{l}</p>)}
+                                            </div>
+                                        )
+                                    })}
+
+                                </span>
+                            )
+                        })}
+                        </span>
+                    )
+                })}
+            </div>
+        )
+    }
 }
 
 export default Tabs;
