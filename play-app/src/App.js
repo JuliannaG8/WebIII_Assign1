e38619a5305a53f2b2319c10932e90a9155bdd3f
Julianna <gagliardijulianna@gmail.com>
2021-11-09 12:49:45 -0700

    fixed routing

--- play-app/src/App.js
+++ play-app/src/App.js
@@ -119,7 +119,7 @@ function App() {
         return (
             <div className="App">
 
-                <Route path="/WebIII_Assign1/" exact>
+                <Route path="/WebIII_Assign1/" >
                     <HomeView songs={fullPlaysList} />
                 </Route>
                 <Route path="/HomeView" exact>
